a71d8ce4ccb4ec832ed4a62542b41e1b38d37192
Zahra Sarabi <zahrasarabi1379@gmail.com>
2024-04-11 01:10:50 +0330

    feat(setup):separat type from files

---
+++ src/style/componentsType.ts
@@ -0,0 +1,12 @@
+export interface ILoginFormData {
+  phone_number: number;
+}
+
+export interface IVerifyCodeData {
+  code: number;
+  phone_number: string;
+}
+
+export interface VerifyFormProps {}
+
+export interface VerifyNumberFormProps {}
