984fefd6e9cf65661278006ab7c29d1bc316800b
Zahra Sarabi <zahrasarabi1379@gmail.com>
2024-04-12 01:12:12 +0330

    fix(main-page):handle the problem in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import React from "react";
 import VerifyNumberForm from "@/components/pages/main-page/VerifyNumberForm";
+import { CustomCard } from "@/style/styled-components/FormCard";
 
 const page = () => {
-  return <VerifyNumberForm />;
+  return (
+    <CustomCard>
+      <VerifyNumberForm />
+    </CustomCard>
+  );
 };
 
 export default page;
